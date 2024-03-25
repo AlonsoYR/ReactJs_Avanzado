dc664832eebf839be15f6b9f6e322a91c7c2d269
AlonsoYR <alonso001029@gmail.com>
2024-03-25 18:56:19 -0500

    Video 13 Terminado

--- ReactAvanced/finalproyect/jest.config.js
+++ ReactAvanced/finalproyect/jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
     transform: {
         "^.+\\.jsx?$": "babel-jest",
-        '^.+\\.(js|jsx|svg)$': '<rootDir>/node_modules/react-svg/transform.js',
     },
     testEnvironment: "jsdom",
-    moduleNameMapper: {
-        '\\.(svg)$': '<rootDir>/__mocks__/svg.js',
-      },
 }
