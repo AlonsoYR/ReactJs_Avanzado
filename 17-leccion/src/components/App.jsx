98ffa48d8d9cc7443b75142269fcddecec4fea0a
AlonsoYR <alonso001029@gmail.com>
2024-04-01 19:43:23 -0500

    Video 17 Avance

---
+++ 17-leccion/src/components/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import TaskList from "./lists/TaskList";
+import '../index.css';
+import Settings from "./settings/Settings";
+
+/**
+*Función anonima para crear un Componente Principal
+* @returns {React.Component} Componente principal de nuestra aplicación
+*/
+
+const App = () => {
+    const [dark, setDark] = React.useState(false);
+
+    /**
+     * Documentacion de useEffect
+     * Se crea una variable de estado donde se almacena el valor de la configuración en localStorage    
+     */
+
+    useEffect(() => {
+        const config = JSON.parse(localStorage.getItem("config"));
+        setDark(config.theme);
+    }, []);
+
+    /**
+     * Función para intercambiar la variable de estado light <-> dark
+     */
+    const toggleDark = () => {
+        setDark(!dark);
+    };
+
+    return (
+        <div className={`App ${dark ? "dark" : ""}`}>
+            <TaskList />
+            <hr style={{ marginTop: 20, marginBottom: 20 }} />
+            <Settings toggleDark={toggleDark} />
+        </div>
+    );
+};
+
+export default App;
